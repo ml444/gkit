7224f30e24828ec14bc39f8e1dfef13ab8f7709b
cml <1554714484@qq.com>
2023-04-08 16:49:40 +0800

    fix: ...

--- log/logger.go
+++ log/logger.go
@@ -67,10 +67,10 @@ func Debug(values ...interface{}) { logger.Debug(values...) }
 func Info(values ...interface{})  { logger.Info(values...) }
 func Warn(values ...interface{})  { logger.Warn(values...) }
 func Error(values ...interface{}) { logger.Error(values...) }
-func Fatal(values ...interface{}) { logger.Error(values...) } // In order to print the stack log
+func Fatal(values ...interface{}) { logger.Fatal(values...) } // In order to print the stack log
 
 func Debugf(template string, values ...interface{}) { logger.Debugf(template, values...) }
 func Infof(template string, values ...interface{})  { logger.Infof(template, values...) }
 func Warnf(template string, values ...interface{})  { logger.Warnf(template, values...) }
 func Errorf(template string, values ...interface{}) { logger.Errorf(template, values...) }
-func Fatalf(template string, values ...interface{}) { logger.Errorf(template, values...) }
+func Fatalf(template string, values ...interface{}) { logger.Fatalf(template, values...) } // In order to print the stack log
