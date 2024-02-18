9f6709f274131202141b67ccba5924d278851207
cml <1554714484@qq.com>
2024-02-18 21:27:46 +0800

    refactor: Add multi-language messgae of errorcode

--- errorx/errorx.go
+++ errorx/errorx.go
@@ -4,32 +4,37 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+
 	"google.golang.org/genproto/googleapis/rpc/errdetails"
 	"google.golang.org/grpc/status"
 )
 
-const DefaultErrorCodeLowerLimit = 100000
-
 type ErrCodeDetail struct {
 	StatusCode int32
+	ErrorCode  int32
 	Message    string
+	Polyglot   map[string]string
 }
 
+var lang string
 var errCodeMap = map[int32]*ErrCodeDetail{}
+var defaultError *ErrCodeDetail
 
-func RegisterError(msgMap map[int32]string, codeMap map[int32]int32) {
-	for k, v := range msgMap {
-		detail := ErrCodeDetail{}
-		detail.Message = v
-		if statusCode, ok := codeMap[k]; ok {
-			detail.StatusCode = statusCode
-		} else {
-			if k > DefaultErrorCodeLowerLimit {
-				detail.StatusCode = DefaultStatusCode
-			}
+func SetLang(l string) {
+	lang = l
+}
+
+func RegisterError(codeMap map[int32]*ErrCodeDetail, defaultErr *ErrCodeDetail) {
+	for k, detail := range codeMap {
+		if detail.StatusCode == 0 {
+			detail.StatusCode = DefaultStatusCode
 		}
-		errCodeMap[k] = &detail
+		errCodeMap[k] = detail
 	}
+	if defaultErr.StatusCode == 0 {
+		defaultErr.StatusCode = DefaultStatusCode
+	}
+	defaultError = defaultErr
 }
 
 // Error is a status error.
@@ -39,7 +44,7 @@ type Error struct {
 }
 
 func (e *Error) Error() string {
-	return fmt.Sprintf("error: code = %d errcode = %d message = '%s' metadata = %v cause = %s", e.StatusCode, e.ErrorCode, e.Message, e.Metadata, e.cause)
+	return fmt.Sprintf("error: code=%d errcode=%d message='%s' metadata=%v cause=%s", e.StatusCode, e.ErrorCode, e.Message, e.Metadata, e.cause)
 }
 
 // JSONBytes returns the JSON representation of the error.
@@ -48,10 +53,8 @@ func (e *Error) JSONBytes() []byte {
 	return buf
 }
 
-// Unwrap provides compatibility for Go 1.13 error chains.
 func (e *Error) Unwrap() error { return e.cause }
 
-// Is matches each error in the chain with the target value.
 func (e *Error) Is(err error) bool {
 	if se := new(Error); errors.As(err, &se) {
 		return se.StatusCode == e.StatusCode && se.ErrorCode == e.ErrorCode
@@ -70,21 +73,18 @@ func (e *Error) GetErrorCode() int32 {
 	return e.ErrorInfo.ErrorCode
 }
 
-// WithCause with the underlying cause of the error.
 func (e *Error) WithCause(cause error) *Error {
 	err := Clone(e)
 	err.cause = cause
 	return err
 }
 
-// WithMetadata with an MD formed by the mapping of key, value.
 func (e *Error) WithMetadata(md map[string]string) *Error {
 	err := Clone(e)
 	err.Metadata = md
 	return err
 }
 
-// GRPCStatus returns the Status represented by se.
 func (e *Error) GRPCStatus() *status.Status {
 	s, _ := status.New(ToGRPCCode(int(e.StatusCode)), e.Message).
 		WithDetails(&errdetails.ErrorInfo{
@@ -94,54 +94,68 @@ func (e *Error) GRPCStatus() *status.Status {
 	return s
 }
 
-func New(errCode int32) *Error {
+func pickMsg(detail *ErrCodeDetail) string {
+	msg := detail.Message
+	if detail.Polyglot != nil && lang != "" {
+		if v, ok := detail.Polyglot[lang]; ok {
+			msg = v
+		}
+	}
+	return msg
+}
+
+func getErrDetail(errCode int32) *ErrCodeDetail {
 	detail, ok := errCodeMap[errCode]
 	if !ok {
 		detail = &ErrCodeDetail{}
 		detail.StatusCode = UnknownStatusCode
-		detail.Message = ""
 	}
+	return detail
+}
+
+func New(errCode int32) *Error {
+	detail := getErrDetail(errCode)
 	return &Error{
 		ErrorInfo: ErrorInfo{
 			ErrorCode:  errCode,
 			StatusCode: detail.StatusCode,
-			Message:    detail.Message,
+			Message:    pickMsg(detail),
 		},
 	}
 }
 
 // NewWithStatus new error from errcode and message
 func NewWithStatus(statusCode int32, errCode int32) *Error {
-	detail, ok := errCodeMap[errCode]
-	if !ok {
-		detail = &ErrCodeDetail{}
-		detail.StatusCode = statusCode
-		detail.Message = ""
-	}
+	detail := getErrDetail(errCode)
 	return &Error{
 		ErrorInfo: ErrorInfo{
 			ErrorCode:  errCode,
 			StatusCode: statusCode,
-			Message:    detail.Message,
+			Message:    pickMsg(detail),
 		},
 	}
 }
 
 // NewWithMsg new error from errcode and message
 func NewWithMsg(errCode int32, msg string) *Error {
-	detail, ok := errCodeMap[errCode]
-	if !ok {
-		detail = &ErrCodeDetail{}
-		detail.StatusCode = UnknownStatusCode
-	}
-	detail.Message = msg
+	detail := getErrDetail(errCode)
 	return &Error{
 		ErrorInfo: ErrorInfo{
 			ErrorCode:  errCode,
 			StatusCode: detail.StatusCode,
-			Message:    detail.Message,
+			Message:    msg,
 		},
 	}
+}
+
+func NewDefaultError(statusCode int32, message string) *Error {
+	if statusCode == 0 {
+		statusCode = DefaultStatusCode
+	}
+	if message == "" {
+		message = defaultError.Message
+	}
+	return CreateError(statusCode, defaultError.StatusCode, message)
 }
 
 // CreateError returns an error object for the status code, error code, message.
